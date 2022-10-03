e45575e9feaa80f7dabf3e2a77843867aa437051
Grant <grant.mc.foster@gmail.com>
2022-10-03 14:15:24 -0500

    switch color change event-chosenColor variable to change with color button selection/extra credit-event.target.tagname of TD element only

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const table = document.getElementById("table")
 
 const addRow = document.getElementById("add-row")
 
+let chosenColor = "red"
+
 addRow.onclick = function makeRow() {
     const row = document.createElement("tr")
     for (let i = 0; i < 20; i++) {
@@ -12,11 +14,37 @@ addRow.onclick = function makeRow() {
     table.appendChild(row)
 }
 
+colors.addEventListener("change", function(event) {
+    chosenColor = event.target.value
+});
+
+table.onclick = function colorize(event){
+    if (event.target.tagName === "TD") {
+        const target = event.target
+        if (target.className.length) {
+            target.className = ''
+        } else {
+            target.className = chosenColor
+        }
+    }
+}
+
+/*
 table.onclick = function colorize(event){
     const target = event.target
     if (target.className.length) {
         target.className = ''
     } else {
-        target.className = 'red'
+        target.className = chosenColor
     }
 }
+*/
+
+/* 
+let color = document.getElementById("colors")
+
+color.addEventListener("change", function(event) {
+    console.log(event.target.value)
+    color = event.target.value
+});
+*/
